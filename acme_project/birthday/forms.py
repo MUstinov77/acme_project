c8cfa8f734072be9978c14d558a372b54ce271fb
MUstinov77 <m.ustinov2001@yandex.ru>
2024-09-02 09:07:15 +0300

    working

---
+++ acme_project/birthday/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+
+class BirthdayForm(forms.Form):
+    first_name = forms.CharField(
+        label='Имя',
+        max_length=20
+    )
+    last_name = forms.CharField(
+        label='Фамилия',
+        required=False,
+        help_text='Необязательное поле',
+    )
+    birthday = forms.IntegerField(
+        label='Дата рождения',
+        widget=forms.SelectDateWidget()
+    )
