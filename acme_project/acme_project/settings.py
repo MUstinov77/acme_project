df5dd1fdf343c0f023b2ed3d154f7c7fadea3b86
MUstinov77 <m.ustinov2001@yandex.ru>
2024-09-03 09:55:39 +0300

    new models

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -9,7 +9,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
-    'django-bootstrap5',
+    'django_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
