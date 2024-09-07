a120e292b336e60874d08ab8ec649287e374f299
MUstinov77 <m.ustinov2001@yandex.ru>
2024-09-07 17:47:06 +0300

    login omly actions

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -2,13 +2,14 @@ from django.urls import reverse_lazy
 from django.views.generic import (
     ListView, CreateView, UpdateView, DeleteView, DetailView
 )
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import BirthdayForm
 from .models import Birthday
 from .utils import calculate_birthday_countdown
 
 
-class BirthdayMixin:
+class BirthdayMixin(LoginRequiredMixin):
     model = Birthday
     form_class = BirthdayForm
 
@@ -37,6 +38,6 @@ class BirthdayListView(ListView):
     paginate_by = 10
 
 
-class BirthdayDeleteView(DeleteView):
+class BirthdayDeleteView(LoginRequiredMixin, DeleteView):
     model = Birthday
     success_url = reverse_lazy('birthday:list')
