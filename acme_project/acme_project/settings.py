f41ad8e7849cde7a0d1f47646f7e019019c26a74
Max Ustinov <m.ustinov2001@yandex.ru>
2024-09-07 16:23:37 +0300

    user registration

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from django.conf.global_settings import MEDIA_ROOT, EMAIL_BACKEND, LOGIN_REDIRECT_URL
+from django.conf.global_settings import MEDIA_ROOT, EMAIL_BACKEND, LOGIN_REDIRECT_URL, LOGIN_URL
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -87,6 +87,8 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = 'login'
 
 LOGIN_REDIRECT_URL = 'pages:homepage'
 
